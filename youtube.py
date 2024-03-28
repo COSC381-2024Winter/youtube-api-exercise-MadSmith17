a23ce6728fd0b874791dfdd9866bc35bfc57e225
Madelyn Smith <msmit378@emich.edu>
2024-03-28 02:37:00 +0000

    Modifiyed for loop to look for items and made sure our search_response only searches for video's

--- youtube.py
+++ youtube.py
@@ -9,10 +9,16 @@ YOUTUBE_API_VERSION = "v3"
 
 def youtube_search(query_term, max_results):
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
-    search_response = youtube.search().list(q=query_term, part="id,snippet", maxResults=max_results).execute()
-    return search_response
+    search_response = youtube.search().list(q=query_term, part="id,snippet", maxResults=max_results, type="video").execute()
+    
+    search_list = []
+    for item in search_response['items']:
+        search_list.append(item)
+    
+    return search_list
 
 if __name__ == "__main__":
     query_term = sys.argv[1]
     max_results = sys.argv[2]
     reponse = youtube_search(query_term, max_results)
+    print(reponse)
